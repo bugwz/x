9ed576759745b9222eb46db18878c6f3c010c077
bugwz <cubegwz@gmail.com>
2020-11-24 23:37:55 +0800

    Update  recv and send sh for iterm2

--- macos/iterm2-rzsz/iterm2-send-zmodem.sh
+++ macos/iterm2-rzsz/iterm2-send-zmodem.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+# shellcheck shell=dash
 # Author: Matt Mastracci (matthew@mastracci.com)
 # AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
 # licensed under cc-wiki with attribution required 
 # Remainder of script public domain
 
 osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
-if [[ $NAME = "iTerm" ]]; then
-    FILE=$(osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+if [ "$NAME" = "iTerm" ]; then
+    FILE=$(\
+        osascript -e 'tell application "iTerm" to activate' \
+                  -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' \
+                  -e "do shell script ( \
+                      \"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\" \
+                     )" \
+        )
 else
-    FILE=$(osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+    FILE=$(\
+        osascript -e 'tell application "iTerm2" to activate' \
+                  -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' \
+                  -e "do shell script ( \
+                      \"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\" \
+                     )" \
+        )
 fi
-if [[ $FILE = "" ]]; then
+
+if [ "$FILE" = "" ]; then
     echo Cancelled.
     # Send ZModem cancel
     echo -e \\x18\\x18\\x18\\x18\\x18
@@ -21,5 +35,6 @@ else
     /usr/local/bin/sz "$FILE" --escape --binary --bufsize 4096
     sleep 1
     echo
-    echo \# Received "$FILE"
+    echo
+    echo "# Received $FILE"
 fi
